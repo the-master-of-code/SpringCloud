c46c79ed30021bf58a34efdb1dfa3f171b5085e0
xieyunlong <3272699565@qq.com>
2021-03-05 00:37:21 +0800

    2021/3/5

--- microservice01/src/main/java/com/xyl/microservice01/Microservice01Application.java
+++ microservice01/src/main/java/com/xyl/microservice01/Microservice01Application.java
@@ -3,13 +3,20 @@ package com.xyl.microservice01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RestController
 public class Microservice01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Microservice01Application.class, args);
     }
 
+    @RequestMapping("/hi")
+    public String test(){
+        return "sssss";
+    }
 }
